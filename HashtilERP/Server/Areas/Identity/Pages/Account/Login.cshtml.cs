6629f3cdc19815e74271dfc2d7c7d4553d6761a2
יעקב גיגי <yaccovgigi@hotmail.com>
2021-02-24 09:50:33 +0200

    after publish to azure before fixing slow con and responsive nav bar

--- HashtilERP/Server/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ HashtilERP/Server/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -90,8 +90,9 @@ namespace HashtilERP.Server.Areas.Identity.Pages.Account
                 // To enable password failures to trigger account lockout, set lockoutOnFailure: true
                 var result = await _signInManager.PasswordSignInAsync(Input.Email, Input.Password, Input.RememberMe, lockoutOnFailure: false);
                 var user = await _userManager.FindByEmailAsync(Input.Email.ToString());
-               
-                if(await _userManager.IsInRoleAsync(user, "Thai-Guy"))               
+                await _userManager.UpdateSecurityStampAsync(user);
+
+                if (await _userManager.IsInRoleAsync(user, "Thai-Guy"))               
                 {
                     returnUrl = "~/sowing/insertpassport";
                 }
