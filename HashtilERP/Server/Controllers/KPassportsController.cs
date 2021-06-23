fd071c134576d3699bb67e9674714c0a4bb93562
יעקב גיגי <yaccovgigi@hotmail.com>
2021-06-23 13:07:14 +0300

    fixing in manger new/moving passports error where post raised infinity json pos/neg error(??!!??) changed that to GET to do heavy lifting inside API

--- HashtilERP/Server/Controllers/KPassportsController.cs
+++ HashtilERP/Server/Controllers/KPassportsController.cs
@@ -379,7 +379,7 @@ namespace HashtilERP.Server
             return ChosenList;
         }
 
-
+        //Sowing GET for Insert new passport
         [HttpGet("GetInsertPassport/{passNum}/{growingRoom}")]
         public async Task<ActionResult> GetInsertPassport(int? passNum,string growingRoom)
         {
@@ -454,6 +454,82 @@ namespace HashtilERP.Server
             return CreatedAtAction("GetKPassport", new { id = kPassport.K_PassportId }, kPassport);
         }
 
+        //Mangers GET for Insert new passport(If Sowing did NOT scan at growingRooms)
+        [HttpGet("GetInsertPassportManager/{passNum}/{hamama}/{gamlon}")]
+        public async Task<ActionResult> GetInsertPassportManager(int? passNum, string hamama, string gamlon)
+        {
+            var kPassport = new K_Passport();
+            Passport sap;
+            Oitm sapOitm;
+            int? startingAvg;
+            try
+            {
+                sap = await _context.Passport.Where(X => X.DocNum == passNum).FirstAsync();
+                sapOitm = await _context.Oitm.Where(X => X.ItemCode == sap.UItemCode).FirstAsync();
+
+
+            }
+            //if no passport in SAP
+            catch (Exception)
+            {
+                return StatusCode(500, "NOTFOUND");
+            }
+
+
+            var user = await _userManager.GetUserAsync(User);
+            var screenName = user.ScreenName;
+
+            var dup = await _context.KPassport.Where(X => X.PassportNum == passNum).FirstOrDefaultAsync();
+            //if duplicate in K_Passport
+            if (dup != null)
+            {
+                return StatusCode(500, "DUPLICATE");
+            }
+            DateTime passingDate;
+            kPassport.UserName = screenName.Trim();
+            kPassport.SowDate = sap.UDateSow;
+            passingDate = (DateTime)sap.UDateSow;
+            kPassport.DateEnd = sap.UDateEnd;
+            //if ZIREY LAKOACH
+            if (sap.UQuanOrdP > 5555554) { startingAvg = Convert.ToInt32(sap.UQuanProd * 1000 / sap.UTraySow); } else { startingAvg = Convert.ToInt32(sap.UQuanOrdP * 1000 / sap.UTraySow); }
+            kPassport.PassportStartingAVG = startingAvg; kPassport.GrowingDays = Convert.ToInt32(((TimeSpan)(sap.UDateEnd - sap.UDateSow)).Days);
+            kPassport.OriginalMagashAmount = Convert.ToInt32(sap.UTraySow);
+            kPassport.MagashAmount = Convert.ToInt32(sap.UTraySow);
+            kPassport.PlantsAmount = sap.UQuanProd * 1000;
+            kPassport.PassportStatus = Status.InGreenHouse.Trim();
+            kPassport.PassportStatusCode = (int)PassportStatusCode.InsideGreenHouse;
+            kPassport.ItemCode = sap.UItemCode.Trim();
+            kPassport.SapDocEntry = sap.DocEntry;
+            kPassport.PassportCondition = Status.NotChecked.Trim();
+            kPassport.GrowingRoomExitDay = passingDate.AddDays(Convert.ToInt32(sap.UNights));
+            kPassport.Zan = sap.UZanZl ?? sapOitm.UHebZan ?? sapOitm.UHebGidul;
+            kPassport.CelsTray = Convert.ToInt32(sapOitm.UCelsTray * 1000);
+            kPassport.Gidul = sapOitm.UHebGidul ?? sapOitm.ItemName.Split(new char[] { ' ' })[0];
+            kPassport.IsSavedForCx = false;
+            kPassport.IsNeedToCut = true && sapOitm.ItemName.Contains("מפוצל");
+            kPassport.PassportNum = passNum;
+            kPassport.GrowingRoom = "X";
+            kPassport.Hamama = hamama;
+            kPassport.Gamlon = gamlon;
+            _context.KPassport.Add(kPassport);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                if (KPassportExists(kPassport.K_PassportId))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw new Exception(e.Message);
+                }
+            }
+
+            return CreatedAtAction("GetKPassport", new { id = kPassport.K_PassportId }, kPassport);
+        }
         #endregion
 
         #region PUT
